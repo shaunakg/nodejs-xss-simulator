a62ba8fe2a7a601f3e68beb079fcbe8cab780963
Shaunak G <shaunakg@github.com>
2019-10-16 17:59:34 +1100

    added design elements

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ var io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080;
 const readline = require('readline');
 
-const starting_message = `
-
----
-Welcome to Google Docs Version 2.0
-Type what you want here
----
-
-
-`;
-
 app.get('/', function(req, res){
     console.log("New request to index.html");
     res.sendFile(__dirname + '/index.html');
@@ -30,15 +20,10 @@ app.get('/jquery-1.11.1.js', function(req, res){
     res.sendFile(__dirname + '/jquery-1.11.1.js');
 });
 
-io.emit('stfc', starting_message);
-
 io.on('connection', function(socket){
 
     socket.on('ctfc', function(new_text) {
-
-        console.log("Text changed to: " + new_text);
         socket.broadcast.emit('stfc', new_text);
-
     });
 
 });
