60d93a7ba3d1a4a29a03c94b9ed687c6958aec31
Shaunak G <shaunakg@github.com>
2019-10-17 17:04:01 +1100

    Styling and functionality changes

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080;
 const readline = require('readline');
+var current_text = "";
 
 app.get('/', function(req, res){
-    console.log("New request to index.html");
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -20,10 +20,16 @@ app.get('/jquery-1.11.1.js', function(req, res){
     res.sendFile(__dirname + '/jquery-1.11.1.js');
 });
 
+app.get('/reset', function (req, res) {
+    res.send('--- XSS SIMULATOR API ---\nSent empty server page update. This option is now locked for 1 minute.');
+    io.broadcast.emit('stfc', current_text);
+});
+
 io.on('connection', function(socket){
 
     socket.on('ctfc', function(new_text) {
-        socket.broadcast.emit('stfc', new_text);
+        current_text = new_text;
+        socket.broadcast.emit('stfc', current_text);
     });
 
 });
